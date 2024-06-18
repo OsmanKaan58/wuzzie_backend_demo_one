a6f2f3dc028a3a084248eff83ad1ed8b4cf87215
OsmanKaan58 <osmankaandemirbas58@gmail.com>
2024-06-18 22:55:33 +0300

    initial commit

---
+++ wuzzie_backend_demo_one/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for wuzzie_backend_demo_one project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wuzzie_backend_demo_one.settings')
+
+application = get_wsgi_application()
